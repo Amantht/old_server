b3d19865a723672aa3f238356f500706abcfc1ad
Aman <aman.tht@gmail.com>
2023-01-18 15:12:04 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.post('/insert',(request,response) => {
+    console.log("The client values are:");
+  console.log(request.body);
+  response.send(request.body);
+
+})
+
+
+
+app.listen(8081)
+//localhost:8081
+console.log("server started")
