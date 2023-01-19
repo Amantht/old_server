d7b69e8c7ce91b471723dc42db26354b0a8655a4
Aman <aman.tht@gmail.com>
2023-01-19 07:42:35 +0530

    2023-jan 19

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient } = require('mongodb')
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
+
+const uri = "mongodb+srv://aman:Nepal123@cluster0.aqmkc3t.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+client.connect();
+const db = client.db("Aman");
+const sdp = db.collection("user");
+
 app.post('/insert',(request,response) => {
-    console.log("The client values are:");
-  console.log(request.body);
-  response.send(request.body);
-
+  response.send('YOUR EXPRESS BACKEND IS CONNECTED TO REACT');
+  console.log(request.body)
+  sdp.insertOne(request.body)
+  console.log("Documents Inserted");
+ 
 })
-
 
 
 app.listen(8081)
