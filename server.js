8d77e252161345bd835d9c1b9ec1e3d02055ab54
Aman <aman.tht@gmail.com>
2023-02-16 08:46:15 +0530

    ex5

--- server.js
+++ server.js
@@ -7,19 +7,26 @@ app.use(cors())
 
 const { MongoClient } = require('mongodb')
 
-const uri = "mongodb+srv://aman:Nepal123@cluster0.aqmkc3t.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://admin:admin@cluster0.aqmkc3t.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri);
 client.connect();
-const db = client.db("s16");
-const col = db.collection("user");
 
+// // registration form data
+// const db = client.db("s16");
+// const col = db.collection("user");
+
+
+var db = client.db("S_16");
+var col = db.collection("Registration");
+
+///registration form data
 app.post('/insert',(request,response) => {
   response.send(request.body);
   console.log(request.body)
   col.insertOne(request.body)
   console.log("Documents Inserted");
- 
+
 })
 app.get('/',(request,response) => {
   response.send('This is a Server')
@@ -50,6 +57,14 @@ app.get('/check', (request,response)=> {
   run().catch(console.dir);
 })
 
+// Ex5 form data
+app.post('/insert_stu',(req,res)=>{
+  col =db.collection("Ex5")
+  console.log(req.body)
+  col.insertOne(req.body)
+  res.send(req.body)
+
+})
 app.listen(8081)
 //localhost:8081
 console.log("server started")
